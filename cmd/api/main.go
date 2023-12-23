7b623a27bb0a21ae735fb7b453cd7d4685250921
“Darius” <darwest18@gmail.com>
2023-12-23 07:02:54 -0700

    setting API and OpenAPI specs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,22 +2,32 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 
+	"RESTLibrary/api/router"
 	"RESTLibrary/config"
 )
 
+//  @title          RESTLibrary API
+//  @version        1.0
+//  @description    This is a sample RESTful API with a CRUD
+
+//  @contact.name   Darius West
+//  @contact.url    https://github.com/lFourl
+
+//  @license.name   MIT License
+//  @license.url    not yet
+
+// @host       localhost:8080
+// @basePath   /v1
+
 func main() {
 	c := config.New()
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", hello)
-
+	r := router.New()
 	s := &http.Server{
 		Addr:         fmt.Sprintf(":%d", c.Server.Port),
-		Handler:      mux,
+		Handler:      r,
 		ReadTimeout:  c.Server.TimeoutRead,
 		WriteTimeout: c.Server.TimeoutWrite,
 		IdleTimeout:  c.Server.TimeoutIdle,
@@ -27,8 +37,4 @@ func main() {
 	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatal("Server startup failed")
 	}
-}
-
-func hello(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, world!")
 }
